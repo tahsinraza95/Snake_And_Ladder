eeffb6761e4bfd03c04a55ebbd62ec96218c2285
tahsinraza95 <tahsinr693@gmail.com>
2023-04-12 10:47:32 +0530

    [TahsinRaza] Adding SnakeAndLadder

---
+++ SnakeAndLadder Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder_Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~Welcome To The Snake And Ladder Program~~~~");
+            SnakeAndLadder snakeLadder = new SnakeAndLadder();
+            snakeLadder.Game();
+            Console.ReadLine();
+        }
+    }
+}
