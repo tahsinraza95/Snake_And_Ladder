d10862eae18ff0874a85fb42c0c04114bbae672a
tahsinraza95 <tahsinr693@gmail.com>
2023-04-12 23:41:00 +0530

    [TahsinRaza] Adding No Play Ladder Or Snake

--- SnakeAndLadder Program/SnakeAndLadder.cs
+++ SnakeAndLadder Program/SnakeAndLadder.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder_Program
 {
-    internal class SnakeAndLadder
+    public class SnakeAndLadder
     {
+        internal int PlayerPosition = 0;
+        public void Game()
+        {
+            Random random= new Random();
+            int DieRoll = random.Next(1, 7);
+            PlayerPosition = DieRoll;
+            int Option = random.Next(0, 3);
+            switch (Option)
+            {
+                case 1:
+                    Console.WriteLine("No Play : " + PlayerPosition);
+                    break;
+                case 2:
+                    PlayerPosition += DieRoll;
+                    Console.WriteLine("lader : " + PlayerPosition);
+                    break;
+                case 3:
+                    PlayerPosition -= DieRoll;
+                    Console.WriteLine("Snake : " + PlayerPosition);
+                    break;
+            }    
+        }
     }
 }
